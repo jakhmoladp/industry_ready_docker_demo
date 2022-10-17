7013546aa44171e3286b2514384a11d331be9ecb
Devendra Prasad Jakhmola <=>
2022-10-17 10:37:58 +0000

    updated the main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def welcome_page():
-    return {"message": "Hello Docker!"}
+    return "<h2>Welcome!</h2><p> This is a demo application.</p>"
 
 @app.get("/{username}")
 def welcome_user(username):
-    return {"message": f'Greetings! {username} Ji!. Welcome to this Docker Demo! '}
+    return {"message": f'Greetings! {username}!. Welcome to this Docker Demo! '}
